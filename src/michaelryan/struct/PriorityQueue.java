809d6381b4d50fd32b5866736739ae127faecb98
mehmenmike <mehmenmike@gmail.com>
2019-02-22 20:46:18 +0000

    Documenting!

--- src/michaelryan/struct/PriorityQueue.java
+++ src/michaelryan/struct/PriorityQueue.java
@@ -2,28 +2,58 @@ package michaelryan.struct;
 
 import java.util.Iterator;
 
-public interface PriorityQueue<T>{
+/**
+ * A priority queue.
+ * <p>
+ *     The order of items added to this queue is not retained, such that the element with the highest priority will
+ *     always be the first to be removed. Elements of equal priority are removed in arbitrary order.
+ * </p>
+ * @param <T> the type of elements held in this priority queue
+ */
+public interface PriorityQueue<T> {
 
     /**
-     * Returns true if this priority queue is empty
+     * Returns true if this priority queue is empty.
      *
      * @return true if this priority queue is empty
      */
     boolean isEmpty();
 
     /**
-     * Returns the number of elements in this priority queue
+     * Returns the number of elements in this priority queue.
      *
      * @return the number of elements in this priority queue
      */
     int size();
 
-
+    /**
+     * Inserts an item into this priority queue. A lower number represents a higher priority, with zero as the highest
+     * priority. Negative numbers are treated as zero.
+     *
+     * @param newItem  the item to be added
+     * @param priority the priority of the item
+     */
     void add(T newItem, int priority);
 
+    /**
+     * Returns and removes the item at the front of this priority queue.
+     *
+     * @return the item at the front of this priority queue
+     */
     T dequeue();
 
+    /**
+     * Returns but does not remove the item at the front of this priority queue.
+     *
+     * @return the item at the front of this priority queue
+     */
     T peek();
 
+    /**
+     * Returns an iterator over the elements in this queue. The iterator does not return the elements in any
+     * particular order.
+     *
+     * @return an iterator over the elements in this queue
+     */
     Iterator<T> iterator();
 }
--- src/michaelryan/struct/Queue.java
+++ src/michaelryan/struct/Queue.java
@@ -2,20 +2,56 @@ package michaelryan.struct;
 
 import java.util.Iterator;
 
+/**
+ * A first-in first-out queue.
+ * <p>
+ *     The order of items added to this queue is retained, such that the first element added will be the first to
+ *     be removed.
+ * </p>
+ * @param <T> the type of elements held in this queue
+ */
 public interface Queue<T> {
+
     /**
-     * Returns true if this
-     * @return
+     * Returns true if this queue is empty.
+     *
+     * @return true if this queue is empty
      */
     boolean isEmpty();
 
+    /**
+     * Returns the number of elements in this queue.
+     *
+     * @return the number of elements in this queue
+     */
     int size();
 
+    /**
+     * Inserts an item into this queue.
+     *
+     * @param newItem the item to be added
+     */
     void add(T newItem);
 
+    /**
+     * Returns and removes the item at the front of this queue.
+     *
+     * @return the item at the front of this queue
+     */
     T dequeue();
 
+    /**
+     * Returns but does not remove the item at the front of this queue.
+     *
+     * @return the item at the front of this queue
+     */
     T peek();
 
+    /**
+     * Returns an iterator over the elements in this queue. The iterator does not return the elements in any
+     * particular order.
+     *
+     * @return an iterator over the elements in this queue
+     */
     Iterator<T> iterator();
 }
