d465ba0e7e82033d0ec3eb06b0ffdabe403998c2
mehmenmike <mehmenmike@gmail.com>
2019-05-09 15:20:38 +0100

    document examples

--- src/michaelryan/background/examples/FactorialComputer.java
+++ src/michaelryan/background/examples/FactorialComputer.java
@@ -2,10 +2,18 @@ package michaelryan.background.examples;
 
 import michaelryan.background.TaskCompleter;
 
+/**
+ * An example implementation of {@link TaskCompleter} which calculates the factorial of a given number.
+ */
 public class FactorialComputer extends TaskCompleter<Long> {
 
     private int factorial;
 
+    /**
+     * Constructs a new FactorialComputer with the specified number to find the factorial of.
+     *
+     * @param factorial the number to find the factorial of
+     */
     public FactorialComputer(int factorial){
         super();
         this.factorial = factorial;
--- src/michaelryan/background/examples/Sleeper.java
+++ src/michaelryan/background/examples/Sleeper.java
@@ -2,10 +2,18 @@ package michaelryan.background.examples;
 
 import michaelryan.background.TaskCompleter;
 
+/**
+ * An example implementation of {@link TaskCompleter} which sleeps for the specified amount of time.
+ */
 public class Sleeper extends TaskCompleter {
 
     private long millis;
 
+    /**
+     * Constructs a new Sleeper with the specified number of milliseconds to sleep for.
+     *
+     * @param millis the number of milliseconds to sleep for
+     */
     public Sleeper(long millis){
         super();
         this.millis = millis;
